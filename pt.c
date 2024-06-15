0f0328e3e03478f2ed8af007a8315fb8bbf9373d
shaharg <shaharg@example.com>
2024-06-15 21:50:54 +0300

    feat: first working solution

--- pt.c
+++ pt.c
@@ -1,29 +1,58 @@
+#include <stdio.h>
+
 #include "os.h"
 
-typedef struct node
+#define CHUNK_SIZE 9
+#define OFFSET 12
+#define CHUNK_MASK 0xff800000000
+#define VPN_SIZE 45
+
+
+void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn)
 {
-    struct node** childs;
-} node;
+    uint64_t *node;
+    node = (uint64_t *)phys_to_virt(pt << OFFSET);
+    uint64_t chunk;
 
-void create_mapping(uint64_t, uint64_t , uint64_t );
+    for (int i = 0; i < VPN_SIZE/CHUNK_SIZE; i++)
+    {
+        chunk = (vpn & CHUNK_MASK) >> (VPN_SIZE - CHUNK_SIZE);
+        vpn = vpn << CHUNK_SIZE;
+        if (node[chunk] == 0)
+        {
+            node[chunk] = (alloc_page_frame() << OFFSET);
+        }
+        node = (uint64_t *)phys_to_virt(node[chunk]);
 
-void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn) {
-    if(ppn == NO_MAPPING) {
-        // delete_mapping(pt, vpn);
+    }
+    if(ppn == NO_MAPPING){
+        node[chunk] = ppn;
         return;
     }
-    create_mapping(pt, vpn, ppn);
+    // intreduce offset and set valid to 1
+    node[chunk] = ((ppn<<OFFSET) | 1);
     return;
 }
 
-void create_mapping(uint64_t pt, uint64_t vpn, uint64_t ppn) {
-    node *head = pt;
-    int mask = 0;
-    int chunk = 0;
-    while ( vpn != 0){
-        chunk = vpn & mask;
-        head = create_node(head, chunk);
-        vpn = vpn << 9;
+uint64_t page_table_query(uint64_t pt, uint64_t vpn)
+{
+    uint64_t *node;
+    node = (uint64_t *)phys_to_virt(pt << OFFSET);
+    uint64_t chunk;
 
+    for (int i = 0; i < VPN_SIZE/CHUNK_SIZE; i++)
+    {   
+        chunk = (vpn & CHUNK_MASK) >> (VPN_SIZE - CHUNK_SIZE);
+        vpn = vpn << CHUNK_SIZE;
+        if (node[chunk] == 0)
+        {
+            return NO_MAPPING;
+        }
+        node = (uint64_t*)phys_to_virt(node[chunk]);
     }
+    if (node[chunk] == 0 || node[chunk] == NO_MAPPING)
+    {
+        return NO_MAPPING;
+    }
+    return (node[chunk] >> OFFSET);
 }
